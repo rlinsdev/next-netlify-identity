74ee2a8b2a09f0da92e905a85c096757d63c9c48
Rodrigo Lins <rlinsdev@gmail.com>
2021-05-02 13:51:18 -0300

    add Netlife pkg

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,19 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-export default function Navbar() {
+export default function Navbar () {
   return (
-    <div className="container">
+    <div className='container'>
       <nav>
-        <Image src="/rupee.png" width={50} height={48} />
+        <Image src='/rupee.png' width={50} height={48} />
         <h1>Gaming Vibes</h1>
         <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li><Link href='/'><a>Home</a></Link></li>
+          <li><Link href='/guides'><a>Guides</a></Link></li>
         </ul>
       </nav>
-      <div className="banner">
-        <Image src="/banner.png" width={966} height={276} />
+      <div className='banner'>
+        <Image src='/banner.png' width={966} height={276} />
       </div>
     </div>
   )
